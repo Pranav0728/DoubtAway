20da95faaa52b2390c7da7ab4b8a04fcbd5add68
Pranav0728 <pranavmolawade123@gmail.com>
2023-10-05 20:50:58 +0530

    Version 1.2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import Safety from '../Assets/budget.png'
-import no_Emmison from '../Assets/no_Emmison.png'
-import budget from '../Assets/budget.png'
+import Safety from "../Assets/budget.png";
+import no_Emmison from "../Assets/no_Emmison.png";
+import budget from "../Assets/budget.png";
 import styled from "styled-components";
-
 
 const Card = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-  // const checkNext = () => {
-  //   const labels = document.querySelectorAll('#slider label');
-  //   const nextIndex = selectedIndex === (labels.length - 1) ? 0 : selectedIndex + 1; 
-  //   setSelectedIndex(nextIndex);
-  // }
+  const handleChange = (index) => {
+    setSelectedIndex(index);
+  };
 
-  const check = index => setSelectedIndex(index);
-
-    return (
-      <Wrapper >
+  return (
+    <Wrapper>
       <div>
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
           <div className="md:w-1/4 py-64 md:mb-0 mb-6 flex flex-col text-center items-center">
@@ -36,15 +31,15 @@ const Card = () => {
                 name="slider"
                 id="s1"
                 checked={selectedIndex === 0}
-                onClick={() => check(0)}
+                onChange={() => handleChange(0)}
               />
-              <input 
-                type="radio" 
+              <input
+                type="radio"
                 className="radiobtn"
-                name="slider" 
-                id="s2" 
+                name="slider"
+                id="s2"
                 checked={selectedIndex === 1}
-                onClick={() => check(1)}
+                onChange={() => handleChange(1)}
               />
               <input
                 type="radio"
@@ -52,30 +47,30 @@ const Card = () => {
                 className="radiobtn"
                 id="s3"
                 checked={selectedIndex === 2}
-                onClick={() => check(2)}
+                onChange={() => handleChange(2)}
               />
               <label htmlFor="s1" id="slide1" className="imgCard">
-                <img className="fea" src={Safety} alt=''/>
+                <img className="fea" src={Safety} alt="" />
                 <div className="Comp">
-                    <h1>Safety</h1>
-                    <h4>We can assure you Our Product Is very Safe and easy </h4>
-                    <button>Learn More</button>
+                  <h1>Safety</h1>
+                  <h4>We can assure you Our Product Is very Safe and easy </h4>
+                  <button>Learn More</button>
                 </div>
               </label>
               <label htmlFor="s2" id="slide2" className="imgCard">
-                <img className="fea" src={no_Emmison} alt=''/>
+                <img className="fea" src={no_Emmison} alt="" />
                 <div className="Comp">
-                    <h1>No Emmison</h1>
-                    <h4>We can assure you Our Product Is very Safe and easy </h4>
-                    <button>Learn More</button>
+                  <h1>No Emission</h1>
+                  <h4>We can assure you Our Product Is very Safe and easy </h4>
+                  <button>Learn More</button>
                 </div>
               </label>
               <label htmlFor="s3" id="slide3" className="imgCard">
-                <img className="fea" src={budget} alt=''/>
+                <img className="fea" src={budget} alt="" />
                 <div className="Comp">
-                    <h1>Easily Doubt Away</h1>
-                    <h4>We can assure you Our Product Is very Safe and easy </h4>
-                    <button>Learn More</button>
+                  <h1>Easily Doubt Away</h1>
+                  <h4>We can assure you Our Product Is very Safe and easy </h4>
+                  <button>Learn More</button>
                 </div>
               </label>
             </section>
@@ -87,117 +82,112 @@ const Card = () => {
           </div>
         </div>
       </div>
-      </Wrapper>
-    );
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  input[type="radio"] {
+    visibility: hidden;
+  }
+  .flex {
+    z-index: 20;
   }
 
+  #slider {
+    height: 30vw;
+    width: 40vw;
+    margin: 0 auto;
+    left: -2%;
+    position: relative;
+    perspective: 1000px;
+    transform-style: preserve-3d;
+  }
 
-const Wrapper = styled.section` 
+  #slider label {
+    margin: auto;
+    background-color: aliceblue;
+    width: 50%;
+    height: 90%;
+    border-radius: 50px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    cursor: pointer;
+    transition: transform 0.4s ease;
+  }
 
-input[type="radio"]{
-  visibility:hidden;
- 
-}
-.flex{
-  z-index:20;
-}
+  #s1:checked ~ #slide3,
+  #s2:checked ~ #slide1,
+  #s3:checked ~ #slide2 {
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.3), 0 2px 2px 0 rgba(0, 0, 0, 0.2);
+    transform: translate3d(-50%, 0, -100px);
+  }
 
-#slider {
-  height: 30vw;
-  width: 40vw;
-  margin: 0 auto;
-  left: -2%;
-  position: relative;
-  perspective: 1000px;
-  transform-style: preserve-3d;
-  
-}
+  #s1:checked ~ #slide1,
+  #s2:checked ~ #slide2,
+  #s3:checked ~ #slide3 {
+    box-shadow: 0 13px 25px 0 rgba(0, 0, 0, 0.3),
+      0 11px 7px 0 rgba(0, 0, 0, 0.19);
+    transform: translate3d(0, 0, 0);
+  }
 
-#slider label {
-  margin: auto;
-  background-color: aliceblue;
-  width: 50%;
-  height: 90%;
-  border-radius: 50px;
-  position: absolute;
-  left: 0; right: 0;
-  cursor: pointer;
-  transition: transform 0.4s ease;
-}
+  #s1:checked ~ #slide2,
+  #s2:checked ~ #slide3,
+  #s3:checked ~ #slide1 {
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.3), 0 2px 2px 0 rgba(0, 0, 0, 0.2);
+    transform: translate3d(50%, 0, -100px);
+  }
+  .button1 {
+    position: relative;
+    top: 210px;
+    margin-left: 26%;
+  }
+  .button1 button {
+    padding: 9px 9px;
+    border: none;
+  }
+  .button2 {
+    margin-left: 67.5%;
+    top: -235px;
+    position: relative;
+  }
+  .button2 button {
+    padding: 9px 9px;
+    border: none;
+  }
+  .imgCard img {
+    width: 50%;
+    margin-left: 25%;
+    position: relative;
+    top: 30px;
+  }
+  .Comp {
+    position: relative;
+    top: 50px;
+  }
+  .Comp h1 {
+    position: relative;
+    text-align: center;
+    font-size: 150%;
+  }
 
-#s1:checked ~ #slide3, #s2:checked ~ #slide1,
-#s3:checked ~ #slide2 {
-  box-shadow: 0 6px 10px 0 rgba(0,0,0,.3), 0 2px 2px 0 rgba(0,0,0,.2);
-  transform: translate3d(-50%,0,-100px);
-}
+  .Comp h4 {
+    position: relative;
+    top: 15px;
+    padding: 0px 20px;
+    font-size: 1.2rem;
+  }
+  .Comp button {
+    border: none;
+    border-radius: 8px;
+    color: white;
+    background-color: red;
+    padding: 9px 12px;
+    margin-left: 90px;
+    position: relative;
+    top: 50px;
+  }
+`;
 
-#s1:checked ~ #slide1, #s2:checked ~ #slide2,
-#s3:checked ~ #slide3 {
-  box-shadow: 0 13px 25px 0 rgba(0,0,0,.3), 0 11px 7px 0 rgba(0,0,0,.19);
-  transform: translate3d(0,0,0);
-}
-
-#s1:checked ~ #slide2, #s2:checked ~ #slide3,
-#s3:checked ~ #slide1 {
-  box-shadow: 0 6px 10px 0 rgba(0,0,0,.3), 0 2px 2px 0 rgba(0,0,0,.2);
-  transform: translate3d(50%,0,-100px);
-}
-.button1{
-  position: relative;
-  top: 210px;
-  margin-left: 26%;
-  
-}
-.button1 button{
-  padding: 9px 9px;
-  border: none;
-}
-.button2{
-  margin-left: 67.5%;
-  top: -235px;
-  position: relative;
-}
-.button2 button{
-  padding: 9px 9px;
-  border: none;
-}
-.imgCard img{
-  width: 50%;
-  margin-left: 25%;
-  position: relative;
-  top: 30px;
-}
-.Comp{
-  position: relative;
-  top: 50px;
-}
-.Comp h1{
-  position: relative;
-  text-align: center;
-  font-size: 150%;
-}
-
-.Comp h4{
-  position: relative;
-  top: 15px;
-  padding: 0px 20px;
-  font-size: 1.2rem;
-
-}
-.Comp button{
-  border: none;
-  border-radius:8px ;
-  color: white;
-  background-color: red;
-  padding: 9px 12px;
-  margin-left: 90px;
-  position: relative;
-  top: 50px;
-}
-
-
-
-
-`
-
-export default Card
+export default Card;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,60 +1,57 @@
-
-
 // import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../styles/Button";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 import Card from "./Card.jsx";
 import teacher from "../Assets/teacher.jpg";
 import { useEffect } from "react";
 
-
-
 const HeroSection = () => {
-
-// Onscroll Animation function 
-useEffect(()=> {
-    let observer = new IntersectionObserver((entries) =>{
-       entries.forEach((entry) =>{
-         console.log(entry);
-         if(entry.isIntersecting){
-           entry.target.classList.add('show');
-         }
-         else{
+  // Onscroll Animation function
+  useEffect(() => {
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
           //  entry.target.classList.remove("show")
-         }
-       })
-     } )
+        }
+      });
+    });
 
-     if (typeof document !== 'undefined') {
-       // will run in client's browser only
-       const hiddenElements = document.querySelectorAll(".hidden");
-        
-       hiddenElements.forEach((el) => observer.observe(el));
-   }
+    if (typeof document !== "undefined") {
+      // will run in client's browser only
+      const hiddenElements = document.querySelectorAll(".hidden");
 
-     },[]);
-
+      hiddenElements.forEach((el) => observer.observe(el));
+    }
+  }, []);
 
   return (
     <Wrapper className="bubble">
       <div className="container grid grid-two-column ">
         <div className="section-hero-data">
           <div className="head">
-            <h1 className="hero-heading">Doubt<span1>Away</span1></h1>
-            <div id='Multiples-skills-text'> 
-                    <p> <div className='MultiText'> <Typewriter 
-                                                options={{
-                                                        strings: [
-                                                        'Where curiosity meets guidance', 
-                                                        'The future of e-Learning is wide open!',
-                                                        'Online learning is an important tool'
-                                                        ],
-                                                        autoStart: true,
-                                                        loop: true,
-                                                    }}/></div></p>
-</div>
+            <h1 className="hero-heading">
+              Doubt<span>Away</span>
+            </h1>
+            <div id="Multiples-skills-text">
+                <div className="MultiText">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        "Where curiosity meets guidance",
+                        "The future of e-Learning is wide open!",
+                        "Online learning is an important tool",
+                      ],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </div>
+            </div>
 
             <p className="hero-para">
               DoubtAway help students to solving the doubt by providing a
@@ -80,85 +77,79 @@ useEffect(()=> {
               src={teacher}
               alt="hero image"
               className="hero-img "
-            /> 
+            />
           </picture>
         </div>
       </div>
       <div className="mode">
         <div className="container grid grid-two-column middle hidden">
           <div className="section-hero-image hidden">
-            <Card className="card"/>
+            <Card className="card" />
           </div>
-          
+
           <div className="section-hero-data hidden">
             <div className="head newhead">
               <h2>Why DoubtAway?</h2>
               <p className="hero-para">
                 Your direct path to learning clarity. Connecting students and
-                teachers effortlessly, we are here to ensure every question finds
-                its answer, making your learning journey smoother than ever.
+                teachers effortlessly, we are here to ensure every question
+                finds its answer, making your learning journey smoother than
+                ever.
               </p>
             </div>
           </div>
         </div>
         <div className="wave-container">
-      <div className="wave"></div>
-     </div>
+          <div className="wave"></div>
+        </div>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
-
-@keyframes fade-up {
-  0%{
+  @keyframes fade-up {
+    0% {
       opacity: 0;
       transform: translateY(30px) scale(0.9);
-  }
+    }
 
-
-
-  100%{
+    100% {
       opacity: 1;
       transform: rotateY(0px) scale(1);
+    }
+    .card {
+      z-index: 10;
+    }
   }
-  .card{
-   z-index:10; 
+  .wave-container {
+    height: 2%;
+    width: 100%;
+    position: absolute;
+    //bottom: 50;
+    left: 0;
+    background-color: ${({ theme }) => theme.colors.helper};
   }
-}
-.wave-container {
-  height: 2%;
-  width: 100%;
-  position: absolute;
-  //bottom: 50;
-  left: 0;
-  background-color: ${({ theme }) => theme.colors.helper};
 
-}
-
-.wave {
-  background: url(https://ilumina.ca/img/blue_bg_02.png)
-  repeat-x;
-  position: absolute;
-  top: -198px;
-  width: 6400px;
-  height: 300px;
-  // animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
-  transform: translate3d(0, 0, 0);
-  z-index:0;
-  
-}
-
-
-@keyframes wave {
-  0% {
-     margin-left: 0;
+  .wave {
+    background: url(https://ilumina.ca/img/blue_bg_02.png) repeat-x;
+    position: absolute;
+    top: -198px;
+    width: 6400px;
+    height: 300px;
+    // animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translate3d(0, 0, 0);
+    z-index: 0;
   }
-  100% {
-     margin-left: -1500px;
+
+  @keyframes wave {
+    0% {
+      margin-left: 0;
+    }
+    100% {
+      margin-left: -1500px;
+    }
   }
-}
 
   padding: 9rem 0;
 
@@ -181,86 +172,75 @@ const Wrapper = styled.section`
     font-size: 2rem;
     color: ${({ theme }) => theme.colors.helper};
   }
-  
-  span1{
-    font-family: 'Inter', sans-serif;
-   
+
+  span1 {
+    font-family: "Inter", sans-serif;
   }
- .btn{
+  .btn {
     animation: fade-up 1s;
   }
 
-  #Multiples-skills-text p{
+  #Multiples-skills-text p {
     font-size: 1.2rem;
     display: flex;
     flex-wrap: nowrap;
     gap: 7px;
-    font-family: 'Inter', sans-serif;
-
-}
-.MultiText{
-  color:yellow;
-  margin-top: 0.5rem;
-  font-size: 3rem;
- 
- 
-}
+    font-family: "Inter", sans-serif;
+  }
+  .MultiText {
+    color: yellow;
+    margin-top: 0.5rem;
+    font-size: 3rem;
+  }
 
   .container {
     height: 90vh;
   }
-  h2{
-    font-weight:500;
+  h2 {
+    font-weight: 500;
   }
 
   .hero-heading {
     font-size: 7rem;
-    color:white;
-    font-family: 'Kalam', cursive;
+    color: white;
+    font-family: "Kalam", cursive;
   }
 
-  .head{
+  .head {
     animation: fade-up 1s;
-
   }
   .hero-para {
     margin-top: 1rem;
     margin-bottom: 3.4rem;
     width: 60rem;
-    color:white;
+    color: white;
     font-size: 1.8rem;
-   
   }
   .mode {
     background-color: #f8f9fa;
-    margin-top:-2rem;
+    margin-top: -2rem;
     padding: 10rem;
   }
   .section-hero-image {
     display: flex;
     justify-content: center;
     align-items: center;
-   
   }
-  .section-hero-image img{
-    
+  .section-hero-image img {
   }
 
   picture {
     text-align: center;
-   
   }
 
   .hero-img {
     max-width: 80%;
-    border-radius:20px;
+    border-radius: 20px;
     animation: fade-up 1s;
   }
-  .newhead p{ 
-    color:black;
+  .newhead p {
+    color: black;
   }
-
-
 
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     .grid {
